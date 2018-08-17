0a3e6e19534f6c24b646f5dcefa7d2c1cb3a6078
William Bodily <william.bodily@gmail.com>
2018-08-16 23:02:39 -0400

    Initial project setup

---
+++ mvc-ifyme-capn/Controller/Controller.swift
@@ -0,0 +1,25 @@
+//
+//  Controller.swift
+//  mvc-ifyme-capn
+//
+//  Created by William Bodily on 8/16/18.
+//  Copyright © 2018 William Bodily. All rights reserved.
+//
+
+import UIKit
+
+class Controller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
