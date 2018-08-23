c29ab3003eabf52bd8d64b04cc860287c3c8e69b
William Bodily <william.bodily@gmail.com>
2018-08-22 22:25:25 -0400

    Created AppleProduct model layer

---
+++ mvc-ifyme-capn/Model/AppleProduct.swift
@@ -0,0 +1,21 @@
+//
+//  AppleProduct.swift
+//  mvc-ifyme-capn
+//
+//  Created by William Bodily on 8/20/18.
+//  Copyright © 2018 William Bodily. All rights reserved.
+//
+
+import Foundation
+
+class AppleProduct {
+    var name: String
+    var color: String
+    var price: Double
+    
+    init(name: String, color: String, price: Double) {
+        self.name = name
+        self.color = color
+        self.price = price
+    }
+}
