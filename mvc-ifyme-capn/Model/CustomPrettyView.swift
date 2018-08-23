173c6b1d218b862afca13f7feffc1b82e23d43bc
William Bodily <william.bodily@gmail.com>
2018-08-22 22:46:14 -0400

    Added CustomPrettyView view layer

---
+++ mvc-ifyme-capn/Model/CustomPrettyView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomPrettyView.swift
+//  mvc-ifyme-capn
+//
+//  Created by William Bodily on 8/22/18.
+//  Copyright © 2018 William Bodily. All rights reserved.
+//
+
+import UIKit
+
+class CustomPrettyView: UIView {
+    override func awakeFromNib() {
+        layer.cornerRadius = 20
+        layer.shadowColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        layer.shadowRadius = 10
+        layer.shadowOpacity = 0.75
+        backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+        layer.borderColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        layer.borderWidth = 5
+    }
+}
